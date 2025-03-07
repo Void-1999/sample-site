d3e62d01c5e9315cd319bad312cad2817184b3a3
void210210@gmail.com <void210210@gmail.com>
2025-03-07 16:02:37 +0530

    ......

--- forrest-of-us/pages/_app.js
+++ forrest-of-us/pages/_app.js
@@ -1,7 +1,24 @@
 import { MotionConfig, AnimatePresence } from 'framer-motion';
+import { useRouter } from 'next/router';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(`App route changed to ${url}`);
+    };
+
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <MotionConfig reducedMotion="user">
       <AnimatePresence exitBeforeEnter>
