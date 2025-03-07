417279d665f18fc0dd3100bcf6f7fba490fabe96
void210210@gmail.com <void210210@gmail.com>
2025-03-07 15:05:06 +0530

    idk man

--- forrest-of-us/pages/index.js
+++ forrest-of-us/pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url(/background.webp)' }}>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center" style={{ backgroundImage: 'url(/background.jpg)' }}>
       <div className="text-center">
         <h1 className="text-6xl font-bold text-white mb-4">Turning Waste into Opportunity</h1>
         <p className="text-xl text-white mb-8">One Step at a Time</p>
