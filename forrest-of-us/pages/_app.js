4032d31ee9f60a51d286bafcc89b09762edfb9b4
AkhandAwara <101900290+AkhandAwara@users.noreply.github.com>
2025-05-09 14:45:54 +0530

    add tree loading animation

--- forrest-of-us/pages/_app.js
+++ forrest-of-us/pages/_app.js
@@ -1,37 +1,44 @@
 // pages/_app.js
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { MotionConfig, AnimatePresence } from 'framer-motion';
 import { useRouter } from 'next/router';
 import Header from '../components/Header';
+import SplashScreen from '../components/SplashScreen'; 
 import '../styles/globals.css';
 import 'leaflet/dist/leaflet.css';
 
 function MyApp({ Component, pageProps }) {
- const router = useRouter();
+  const router = useRouter();
+  const [showMain, setShowMain] = useState(false);
 
- useEffect(() => {
-  const handleRouteChange = (url) => {
-   console.log(`App route changed to ${url}`);
-  };
-  router.events.on('routeChangeStart', handleRouteChange);
-  router.events.on('routeChangeComplete', handleRouteChange);
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(`App route changed to ${url}`);
+    };
+    router.events.on('routeChangeStart', handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
 
-  return () => {
-   router.events.off('routeChangeStart', handleRouteChange);
-   router.events.off('routeChangeComplete', handleRouteChange);
-  };
- }, [router.events]);
+    return () => {
+      router.events.off('routeChangeStart', handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
- return (
-  <MotionConfig reducedMotion="user">
-      <AnimatePresence exitBeforeEnter>
-        <main className="mt-16"> {/* Add a margin to push content below the header */}
-          <Component {...pageProps} key={router.route} />
-          <Header />
-        </main>
-      </AnimatePresence>
-    </MotionConfig>
- );
+  return (
+    <>
+      {!showMain && <SplashScreen onComplete={() => setShowMain(true)} />}
+      {showMain && (
+        <MotionConfig reducedMotion="user">
+          <AnimatePresence mode="wait">
+            <main className="mt-16">
+              <Component {...pageProps} key={router.route} />
+              <Header />
+            </main>
+          </AnimatePresence>
+        </MotionConfig>
+      )}
+    </>
+  );
 }
 
 
